897e5ed1cd6452b698470be6f87d589a463029d3
WojtekM <wojciech.mardyla@gmail.com>
2012-07-11 18:05:35 +0200

    Init for mhapp

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mhapp::Application.config.secret_token = '8b83fe3340280ec5c55e76558ff814cb44713f3d9266e58223e6eb950bb3c77d99299a0d896bb4631e2fa81f76285056081a8ea1f18a741850219ec854ca75c2'
